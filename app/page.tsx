84fd3dc88144c523bf903b234a3d5866a3a0cabe
hemant069 <hemantprajapati20500@gmail.com>
2025-01-03 18:54:42 +0530

    design image is added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
 import { Toaster } from "react-hot-toast";
 import Card from "./components/Card";
+import Image from "next/image";
+import design from "../public/exceildraw.svg";
 
 const page = () => {
   return (
     <div className="p-32">
       <Toaster position="top-center" />
+      <div className="mb-10 flex justify-center ">
+        <Image
+          className="opacity-80 rounded-3xl object-fill bg-fixed"
+          src={design}
+          alt="design"
+          width={600}
+          height={300}
+        />
+      </div>
       <div className=" border h-[30rem]  p-2 rounded-xl ">
         <Card />
       </div>
